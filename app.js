c349df1979f7319fef38613b32bb5d0a6bda006b
Kewyn Akshlley <kewyn.akshlley@dcx.ufpb.br>
2020-09-08 20:32:37 -0300

    removing pkey

--- app.js
+++ app.js
@@ -10,8 +10,7 @@ const geoKey = process.env.GEO_KEY
 const weatherUrl = "http://api.weatherstack.com/current?access_key="+weatherKey+"&query=-6.806900,-35.077759"
 const geoUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token="+geoKey
 
-request({url: weatherUrl = "http://api.weatherstack.com/current?access_key=f2e46a8dba3fed054932ea8ed3c52f58&query=-6.806900,-35.077759"
-, json: true}, (error, response) => {
+request({url: weatherUrl, json: true}, (error, response) => {
     console.log("It's currently " + response.body.current.temperature + " degrees. It feels like "+response.body.current.feelslike)
     console.log(response.body.current.weather_descriptions[0])
 })
