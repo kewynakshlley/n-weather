5074aa115a0f42b5e73f1a4ccc23a44e59a037f2
Kewyn Akshlley <kewyn.akshlley@dcx.ufpb.br>
2020-09-09 00:57:14 -0300

    adding callback to the forecast function and isolating the function

--- app.js
+++ app.js
@@ -1,34 +1,18 @@
-const request = require('request')
-const dotenv = require('dotenv');
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
 
-dotenv.config();
-
-const weatherKey = process.env.WEATHER_KEY
-const geoKey = process.env.GEO_KEY
-
-
-const weatherUrl = "http://api.weatherstack.com/current?access_key=" + weatherKey + "&query=-6.806900,-35.077759"
-const geoUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/Rio%20Tinto.json?access_token=" + geoKey
-
-// request({ url: weatherUrl, json: true }, (error, response) => {
-//     if (error) {
-//         console.log('Unable to connect to the weather API..')
-//     } else if (response.body.error) {
-//         console.log("Unable to find location.")
-//     } else {
-//         console.log("It's currently " + response.body.current.temperature + " degrees. It feels like " + response.body.current.feelslike)
-//         console.log(response.body.current.weather_descriptions[0])
-//     }
-// })
-
-request({ url: geoUrl, json: true }, (error, response) => {
+forecast(-71.0596, 42.3605, (error, data) => {
     if (error) {
-        console.log('Unable to connect to the geo API..')
-    }else if(response.body.features.length === 0){
-        console.log('Unable to find coordinates.')
-    }else {
-        const center = response.body.features[0].center
-        console.log("latitude: " + center[0] + "\nlongitude: " + center[1])
+        console.log(error);
+    } else {
+        console.log(data);
     }
+})
 
+geocode("Boston", (error, data) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(data);
+    }
 })
---
+++ utils/forecast.js
@@ -0,0 +1,25 @@
+const request = require("request");
+const secrets = require("./secrets");
+
+const weatherKey = secrets.weatherKey;
+const weatherUrl =
+    "http://api.weatherstack.com/current?access_key=" + weatherKey + "&query=";
+
+const forecast = (latitude, longitude, callback) => {
+    const finalURL = weatherUrl + latitude + "," + longitude
+    request({ url: finalURL, json: true }, (error, response) => {
+        if (error) {
+            callback("Unable to connect to the weather API..", undefined);
+        } else if (response.body.error) {
+            callback("Unable to find location.", undefined);
+        } else {
+            callback(undefined, {
+                temperature: response.body.current.temperature,
+                feelslike: response.body.current.feelslike,
+                weather_description: response.body.current.weather_descriptions[0],
+            })
+        }
+    })
+}
+
+module.exports = forecast;
