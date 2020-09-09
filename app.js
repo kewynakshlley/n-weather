2aa69ae230137c21c80b1b0d1f44d173d360d1d1
Kewyn Akshlley <kewyn.akshlley@dcx.ufpb.br>
2020-09-09 01:39:03 -0300

    commandline args

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 const geocode = require("./services/geocode");
 const forecast = require("./services/forecast");
 
-geocode("João Pessoa", (error, data) => {
-    if(error){
-        return console.log(error);
-    }
+const address = process.argv[2]
 
-    forecast(data.latitude, data.longitude, (error, forecastData) => {
-        if (error) {
+if (address) {
+    geocode(address, (error, data) => {
+        if(error){
             return console.log(error);
         }
 
-        console.log(data.location);
-        console.log("Temperature of " + forecastData.temperature +" Cº.")
-        console.log("Feelslike " + forecastData.feelslike + " Cº.")
-        console.log(forecastData.weather_description)
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error);
+            }
 
+            console.log(data.location);
+            console.log("Temperature of " + forecastData.temperature +" Cº.")
+            console.log("Feelslike " + forecastData.feelslike + " Cº.")
+            console.log(forecastData.weather_description)
+
+        })
     })
-})
+} else {
+    console.log('Location not provided. (node app.js "LOCATION")')
+}
+
+
