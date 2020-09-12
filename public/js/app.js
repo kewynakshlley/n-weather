d9427ccb4d7750927d6a327a4188c6dbd014596e
Kewyn Akshlley <kewyn.akshlley@dcx.ufpb.br>
2020-09-12 00:32:50 -0300

    search form and basic user interface

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,28 @@
-console.log('clientside js')
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+const messageOne = document.querySelector('#p1')
+const messageTwo = document.querySelector('#p2')
+const messageThree = document.querySelector('#p3')
+const messageFour = document.querySelector('#p4')
+
+
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const location =  search.value
+
+    fetch('http://localhost:3000/weather?city=' + location).then((response) => {
+    response.json().then((data) =>{
+        if(data.error){
+            messageOne.textContent = data.error
+        }else{
+            messageOne.textContent = data.city
+            messageTwo.textContent = "Temperature of " + data.temperature + "Cº"
+            messageThree.textContent = "Feelslike " + data.feelslike + "Cº"
+            messageFour.textContent = "Weather description: " + data.weather_description
+        }
+
+    })
+})
+})
