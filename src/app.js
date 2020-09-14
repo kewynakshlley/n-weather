491d554a1d31064d4086650ed84b35db1e632c03
Kewyn Akshlley <kewyn.akshlley@dcx.ufpb.br>
2020-09-14 16:54:59 -0300

    dinamic port

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require("./services/geocode");
 const forecast = require("./services/forecast");
 
 const app = express()
+const port = process.env.PORT || 3000;
 
 // Define paths for Express config
 const pathDir = path.join(__dirname, '../public')
@@ -90,7 +91,7 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server running on port 3000.')
+app.listen(port, () => {
+    console.log('Server running on port ' + port)
 })
 
